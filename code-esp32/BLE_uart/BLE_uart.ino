07f1915ab4407be35b8ae1bc1b73a1c7ef80b0b5
JD <zhangdi_jd@cau.edu.cn>
2022-02-09 17:20:06 +0800

    实现每秒向上位机发送字符串

--- code-esp32/BLE_uart/BLE_uart.ino
+++ code-esp32/BLE_uart/BLE_uart.ino
@@ -26,9 +26,11 @@
 
 BLEServer *pServer = NULL;
 BLECharacteristic * pTxCharacteristic;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint8_t txValue = 0;
+bool deviceConnected = false; // 当前有无设备连接
+bool oldDeviceConnected = false; // 是否已经有设备连接
+std::string txValue = "";
+ulong current_millis = 0;
+ulong send_millis = 0;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -37,9 +39,11 @@ uint8_t txValue = 0;
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
+#define period_millis 1000 // 发信间隔
 
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
+
+class MyServerCallbacks: public BLEServerCallbacks {  // 调用成员函数修改设备连接状态
+    void onConnect(BLEServer* pServer) { 
       deviceConnected = true;
     };
 
@@ -48,14 +52,14 @@ class MyServerCallbacks: public BLEServerCallbacks {
     }
 };
 
-class MyCallbacks: public BLECharacteristicCallbacks {
+class MyCallbacks: public BLECharacteristicCallbacks { // 将接收的字符串按字符逐个打印出来
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string rxValue = pCharacteristic->getValue();
 
-      if (rxValue.length() > 0) {
+      if (rxValue.length() > 0) { // 若接收到的字符串不为空
         Serial.println("*********");
         Serial.print("Received Value: ");
-        for (int i = 0; i < rxValue.length(); i++)
+        for (int i = 0; i < rxValue.length(); i++) // 则逐个输出
           Serial.print(rxValue[i]);
 
         Serial.println();
@@ -64,6 +68,26 @@ class MyCallbacks: public BLECharacteristicCallbacks {
     }
 };
 
+void overFlow()
+{
+  // 溢出函数
+  // TODO
+}
+
+void sendMsg(std::string msg_to_TX)
+{
+  // 将字符串发出去
+  if (msg_to_TX.length() <= ESP_GATT_MAX_ATTR_LEN)
+  {
+    pTxCharacteristic->setValue(msg_to_TX);
+    pTxCharacteristic->notify();
+  }
+  else{
+    Serial.println("Length ERROR!");
+    // 将超过六百字的字符串分两次发过去
+    // TODO
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -73,53 +97,69 @@ void setup() {
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+  pServer->setCallbacks(new MyServerCallbacks()); // 将连接状态响应函数设置为第一个类
 
   // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLEService *pService = pServer->createService(SERVICE_UUID); // 设置设备的唯一蓝牙标识码
 
   // Create a BLE Characteristic
   pTxCharacteristic = pService->createCharacteristic(
-										CHARACTERISTIC_UUID_TX,
-										BLECharacteristic::PROPERTY_NOTIFY
+										CHARACTERISTIC_UUID_TX, // 发信通道
+										BLECharacteristic::PROPERTY_NOTIFY // 发信方法为notify
 									);
                       
   pTxCharacteristic->addDescriptor(new BLE2902());
 
   BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
-											 CHARACTERISTIC_UUID_RX,
-											BLECharacteristic::PROPERTY_WRITE
+											 CHARACTERISTIC_UUID_RX, // 收信通道
+											BLECharacteristic::PROPERTY_WRITE // 收信方法为write
 										);
 
-  pRxCharacteristic->setCallbacks(new MyCallbacks());
+  pRxCharacteristic->setCallbacks(new MyCallbacks()); // 设置响应函数
 
   // Start the service
-  pService->start();
+  pService->start(); // 启动服务
 
   // Start advertising
-  pServer->getAdvertising()->start();
+  pServer->getAdvertising()->start(); // 开始广播蓝牙
   Serial.println("Waiting a client connection to notify...");
+
+  current_millis = millis();
+  send_millis = current_millis;
 }
 
 void loop() {
 
-    if (deviceConnected) {
-        pTxCharacteristic->setValue(&txValue, 1);
-        pTxCharacteristic->notify();
-        txValue++;
-		delay(10); // bluetooth stack will go into congestion, if too many packets are sent
+  // 如果蓝牙发送过于频繁，会导致通信阻塞
+
+  if (deviceConnected) {
+		// delay(1000); // bluetooth stack will go into congestion, if too many packets are sent
+    current_millis = millis();
+    if (current_millis - send_millis >= period_millis)
+    {
+      send_millis = current_millis;
+      std::string msg_to_TX = "Hello World!"; // TODO
+      sendMsg(msg_to_TX);
+    }
+    else if (current_millis < send_millis)
+    {
+      overFlow();
+    }
 	}
 
-    // disconnecting
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
-        Serial.println("start advertising");
-        oldDeviceConnected = deviceConnected;
-    }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
-		// do stuff here on connecting
-        oldDeviceConnected = deviceConnected;
-    }
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) { 
+    // 当前无设备连接，且ESP32记忆设备连接状态的标志位尚未更改，意思是已连接设备掉线了
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected; // 将已经记忆的状态更新
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+    // 新连接的设备进来之后对标志位进行更新
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+    Serial.println("device connected");
+  }
 }
