7c94231bbedc6af5297126e5722e20b98189e088
kearney <191615342@qq.com>
2022-02-10 16:15:07 +0800

    时间戳和中文乱码

--- wechatapp/pages/blueTooth/blueTooth.js
+++ wechatapp/pages/blueTooth/blueTooth.js
@@ -10,9 +10,12 @@ function stringToBytes(str) {
   return array.buffer;
 }
 
-/** ArrayBuffer转 utf8 字符串 */
+/** ArrayBuffer转 utf8 字符串  */
 function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint8Array(buf));
+  let encodedString = String.fromCodePoint.apply(null, new Uint8Array(buf));
+  let decodedString = decodeURIComponent(escape(encodedString)); //没有这一步中文会乱码
+  console.log(decodedString);
+  return decodedString
 }
 
 
@@ -153,7 +156,11 @@ Page({
       characteristicId,
       success(res) {
         wx.onBLECharacteristicValueChange(function (res) {
-          var str = ab2str(res.value);
+          var jsonstr = ab2str(res.value);
+          var jsonobj = JSON.parse(jsonstr); // 如果不是合理的格式会出错，处理 to do
+          var today = new Date();
+          jsonobj.time=today.toLocaleString();  // 加入当地时间戳
+          var str = JSON.stringify(jsonobj);
           console.log(str);
           that.setData({
             msg: str
