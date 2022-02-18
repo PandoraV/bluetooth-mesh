289c114a8da06db9d58833792c5ea76e828574ad
JD <zhangdi_jd@cau.edu.cn>
2022-02-19 01:00:49 +0800

    补充版权信息

--- code-esp32/main/main.ino
+++ code-esp32/main/main.ino
@@ -134,5 +134,8 @@
    Please support us by buying our products (and not the clones) from
    https://thingpulse.com
    
+  软串口部分代码感谢：
+  版权声明：本文为CSDN博主「xiaoshihd」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+  原文链接：https://blog.csdn.net/xiaoshihd/article/details/109398264
 */
   
