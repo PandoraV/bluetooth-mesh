18f1c60d224967316534689f529e553d77f7796a
JD <zhangdi_jd@cau.edu.cn>
2023-05-09 14:45:04 +0800

    改逻辑

--- code-esp32/tongxin/tongxin.ino
+++ code-esp32/tongxin/tongxin.ino
@@ -23,8 +23,8 @@ void loop() {
   delay(1000);
 }
 // 读取PZEM数据
-float readPZEMData(byte cmd, byte regHi, byte regLo, byte lenHi, byte lenLo, byte num, byte len) {
-  byte request[8] = {0xA1, cmd, regHi, regLo, lenHi, lenLo, 0x00, 0x00};
+float readPZEMData(byte cmd, byte regHi, byte regLo, byte lenHi, byte lenLo, byte num, int len) {
+  byte request[8] = {0x01, cmd, regHi, regLo, lenHi, lenLo, 0x00, 0x00};
   byte response[7];
   // 计算CRC校验码
   uint16_t crc = crc16(request, 6);
@@ -40,6 +40,8 @@ float readPZEMData(byte cmd, byte regHi, byte regLo, byte lenHi, byte lenLo, byt
     for (int i = 0; i < count; i++) {
       response[i] = MySerial.read();
     }
+  } else {
+    return -1;
   }
   // 解析PZEM回复数据
   if (response[0] == 0xA1 && response[1] == cmd && response[2] == num * len) {
@@ -48,9 +50,7 @@ float readPZEMData(byte cmd, byte regHi, byte regLo, byte lenHi, byte lenLo, byt
       value = value * 256 + response[3 + i];
     }
     return value / 10.0;
-  } else {
-    return -1;
-  }
+  } 
 }
 // 计算CRC校验码
 uint16_t crc16(byte *buf, int len) {
